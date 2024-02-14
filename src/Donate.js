d2cb80d30d01fe584e88ddcfd01bf168cc2bcf01
Vikram Penumarti <vikram.penumarti@gmail.com>
2024-02-14 11:47:58 -0800

    Added nav on other pages

--- src/Donate.js
+++ src/Donate.js
@@ -2,7 +2,16 @@ import './Donate.css';
 
 function Donate() {
   return (
-    <div>Donation Page</div>
+    <div>
+      <nav className="navbar">
+        <a className="logo" href='/'>HelpingHand</a>
+        <div className="nav-links">
+          <a href="/chat">Chat</a>
+          <a href="/resources">Resources</a>
+        </div>
+      </nav>
+      <p className="title">Donate</p>
+    </div>
   );
 }
 
--- src/Resources.js
+++ src/Resources.js
@@ -2,7 +2,16 @@ import './Resources.css';
 
 function Resources() {
   return (
-    <div>Resources Page</div>
+    <div>
+      <nav className="navbar">
+        <a className="logo" href='/'>HelpingHand</a>
+        <div className="nav-links">
+          <a href="/chat">Chat</a>
+          <a href="/donate">Donations</a>
+        </div>
+      </nav>
+      <p className="title">Resources</p>
+    </div>
   );
 }
 
