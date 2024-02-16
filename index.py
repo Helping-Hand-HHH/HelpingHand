be2ac11434646472f8e4af2aa79ededc72224bb4
Vikram Penumarti <vikram.penumarti@gmail.com>
2024-02-16 12:26:01 -0800

    Added audio support and saving state

--- index.py
+++ index.py
@@ -4,7 +4,18 @@ import openai
 from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
+# from langchain.text_splitter import CharacterTextSplitter
+# from langchain.embeddings import OpenAIEmbeddings
+# from langchain.vectorstores import FAISS
+# from langchain.chat_models import ChatOpenAI
+# from langchain.memory import ConversationBufferMemory
+# from langchain.chains import ConversationalRetrievalChain
 
+# class Lang:
+#     conversation = None
+#     chat_history = None
+#     response = None
+    
 class TextRequest(BaseModel):
     text: str
 
@@ -21,6 +32,41 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# def get_text_chunks(text):
+#     text_splitter = CharacterTextSplitter(
+#         separator="\n",
+#         chunk_size=1000,
+#         chunk_overlap=200,
+#         length_function=len
+#     )
+#     chunks = text_splitter.split_text(text)
+#     return chunks
+
+
+# def get_vectorstore(text_chunks):
+#     embeddings = OpenAIEmbeddings()
+#     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+#     return vectorstore
+
+
+# def get_conversation_chain(vectorstore):
+#     llm = ChatOpenAI()
+
+#     memory = ConversationBufferMemory(
+#         memory_key='chat_history', return_messages=True)
+#     conversation_chain = ConversationalRetrievalChain.from_llm(
+#         llm=llm,
+#         retriever=vectorstore.as_retriever(),
+#         memory=memory
+#     )
+#     return conversation_chain
+
+
+# def handle_userinput(user_question):
+#     Lang.response = Lang.conversation({'question': user_question})
+#     Lang.chat_history = Lang.response['chat_history']
+
+
 def generate_response(text):
     response = openai.Completion.create(
         model="gpt-3.5-turbo-instruct",
@@ -32,7 +78,30 @@ def generate_response(text):
 @app.post("/")
 async def return_text(request: TextRequest):
     raw_text = request.text
+    # if not Lang.conversation:
+    #     Lang.conversation = None
+    # if not Lang.chat_history:
+    #     Lang.chat_history = None
+        
+    # handle_userinput(raw_text)
+
+    # text_chunks = get_text_chunks(raw_text)
+
+    # vectorstore = get_vectorstore(text_chunks)
+
+    # Lang.conversation = get_conversation_chain(vectorstore)
+    
     try:
+        # return {"message": generate_response(raw_text), "conversation": Lang.conversation, "chat_history": Lang.chat_history}
         return {"message": generate_response(raw_text)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.post("/audio")
+async def return_text():
+    try:
+        # create function for generating LLM response on request with whisper
+        # return the response generated
+        return {"message": "Audio recording successfully sent"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,31 @@
 import './Chat.css';
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { FaMicrophone } from "react-icons/fa";
 
 function Chat() {
   const [text, setText] = useState('');
   const [conversation, setConversation] = useState([]);
   const [response, setResponse] = useState('Waiting for Submission');
 
+  useEffect(() => {
+    const storedConvo = localStorage.getItem('convo');
+    console.log(storedConvo);
+    if (storedConvo) {
+      setConversation(JSON.parse(storedConvo));
+    }
+  }, []);
+
+  const clearStorage = () => {
+    localStorage.removeItem('convo');
+    setConversation('');
+  };
+
   const updateConversation = (userText, botResponse) => {
-    setConversation(prevConvo => [...prevConvo, { type: 'user', message: userText }, { type: 'bot', message: botResponse }]);
+    setConversation(prevConvo => {
+      const updatedConvo = [...prevConvo, userText, botResponse];
+      localStorage.setItem('convo', JSON.stringify([...prevConvo, userText, botResponse]));
+      return updatedConvo;
+    });
   };
 
   return (
@@ -24,18 +42,21 @@ function Chat() {
       <Conversation conversation={conversation} />
       <TextSubmit text={text} setResponse={setResponse} setText={setText} updateConversation={updateConversation}/>
       <p className='response'>{response}</p>
+      <div className='mic'>
+        <AudioRecorder setResponse={setResponse}/>
+      </div>
+      <button className='clear' onClick={clearStorage}>Clear</button>
     </div>
   );
 }
 
 function Conversation({ conversation }) {
 
-
   return (
     <div>
       {conversation.length !== 0 && (<div className='conversation'>
         {conversation.map((convo, index) => (
-          <div className='single-response' key={index}>{convo.type === 'user' ? <span><strong>You:</strong> {convo.message}</span> : <span><strong>HelpingHand:</strong> {convo.message}</span>}</div>
+          <div className='single-response' key={index}>{index % 2 === 0 ? <span><strong>You:</strong> {convo}</span> : <span><strong>HelpingHand:</strong> {convo}</span>}</div>
         ))}
       </div>)}
     </div>
@@ -90,16 +111,90 @@ function TextSubmit({ setResponse, text, setText, updateConversation }) {
   };
 
   return (
-    <div className="text">
-      <textarea
-        ref={textareaRef}
-        className="written"
-        value={text}
-        rows={1}
-        onChange={handleTextChange}></textarea>
-        <input className="button" type='submit' onClick={handleSubmit}></input>
+    <div>
+      <div className="text">
+        <textarea
+          ref={textareaRef}
+          className="written"
+          value={text}
+          rows={1}
+          onChange={handleTextChange}></textarea>
+          <input className="button" type='submit' onClick={handleSubmit}></input>
+      </div>
     </div>
   );
 }
+
+function AudioRecorder({ setResponse }) {
+  const [recording, setRecording] = useState(false);
+  const mediaRecorder = useRef(null);
+  const audioChunks = useRef([]);
+
+  const stopRecording = () => {
+    mediaRecorder.current.stop();
+    setRecording(false);
+  };
+  
+  const sendAudio = async () => {
+    const audioBlob = new Blob(audioChunks.current, { type: 'audio/wav' });
+    audioChunks.current = [];
+  
+    try {
+      const formData = new FormData();
+      formData.append('audio', audioBlob, 'recording.wav');
+  
+      const response = await fetch('http://localhost:8000/audio', {
+        method: 'POST',
+        body: formData,
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+      console.log('Success:', data.message);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+  const startRecording = async () => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaRecorder.current = new MediaRecorder(stream);
+      mediaRecorder.current.ondataavailable = (event) => {
+        audioChunks.current.push(event.data);
+      };
+      mediaRecorder.current.onstop = () => {
+        audioChunks.current = [];
+      };
+      mediaRecorder.current.start();
+      setRecording(true);
+    } else {
+      console.error("getUserMedia not supported on your browser!");
+    }
+  };
+
+  const handleClick = () => {
+    if(recording) {
+      setResponse('Audio recording stopped, generating response');
+      stopRecording();
+      document.getElementById('fa').style.color = 'black';
+      sendAudio();
+    }
+    else {
+      setResponse('Capturing Audio');
+      startRecording();
+      document.getElementById('fa').style.color = 'red';
+    }
+  };
+
+  return (
+    <div id='fa'>
+      <FaMicrophone onClick={handleClick} />
+    </div>
+  );
+};
 
 export default Chat;
